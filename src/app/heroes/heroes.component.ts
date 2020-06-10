d59730a8fb575de8e7294aa4f56e4b4cf8435d30
Marc <marc.marechal@dell.com>
2020-06-10 17:14:27 +0800

    print message when select hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,9 +13,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('HeroService: Select hero id=' + hero.id);
   }
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
